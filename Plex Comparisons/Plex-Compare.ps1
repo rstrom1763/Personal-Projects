43cae89eb9613a34470344e498a8b1c229b607d1
Ryan <48137622+rstrom1763@users.noreply.github.com>
2021-02-27 20:45:53 -0600

    Update Plex-Compare.ps1

--- Plex Comparisons/Plex-Compare.ps1
+++ Plex Comparisons/Plex-Compare.ps1
@@ -1,11 +1,24 @@
 ﻿
-Function Plex-Compare{
+$csv1 = Import-Csv "C:\Users\autob\Downloads\Movies-Level 6-20210102-210814.csv"
+$csv2 = Import-Csv "C:\Users\autob\Downloads\Kent-plex-library-exported - Kent-plex-library-exported.csv"
 
-    param(
+#$csv1 | 
+#Select-Object "Sort title","Year","Bitrate","Video Resolution","Video Codec","Part Size","Part File","Media ID","Title","Duration","Added","Updated","Width","Height","Aspect Ratio","Audio Codec","Container","Part File Combined","Part File Path"
 
-        [Parameter()][string]$csv1,
+$count = 0
+foreach($movie in $csv1){
 
-        [Parameter()][string]$csv2
 
-    )
+    foreach ($movie2 in $csv2){
+
+        if($movie."Sort title" -eq $movie2."Sort title"){
+        
+            $count += 1
+        
+        }
+    
+    }
+
 }
+
+$count
