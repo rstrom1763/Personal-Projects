bb1586150380559cdc99c785b84eda99efafc20d
Ryan <48137622+rstrom1763@users.noreply.github.com>
2021-03-01 22:13:07 -0600

    Update Plex-Compare.ps1
    Now finds movies that do not exist in one of the csv's vs the other.

--- Plex Comparisons/Plex-Compare.ps1
+++ Plex Comparisons/Plex-Compare.ps1
@@ -1,24 +1,37 @@
 ﻿
 $csv1 = Import-Csv "C:\Users\autob\Downloads\Movies-Level 6-20210102-210814.csv"
 $csv2 = Import-Csv "C:\Users\autob\Downloads\Kent-plex-library-exported - Kent-plex-library-exported.csv"
+$need = @()
 
 #$csv1 | 
 #Select-Object "Sort title","Year","Bitrate","Video Resolution","Video Codec","Part Size","Part File","Media ID","Title","Duration","Added","Updated","Width","Height","Aspect Ratio","Audio Codec","Container","Part File Combined","Part File Path"
 
 $count = 0
 foreach($movie in $csv1){
-
+    
+    $found = $false
 
     foreach ($movie2 in $csv2){
 
         if($movie."Sort title" -eq $movie2."Sort title"){
-        
-            $count += 1
+
+            $found = $true
         
         }
+        if($found){
+            
+            break
+
+        }
+    
+    }
+    if($found -eq $false){
+        
+        $need += $movie
+        $count += 1
     
     }
 
 }
-
+$need | Select-Object "Sort title"
 $count
