e05e995fb3dc32a49ff3d0d4594c1bc9c7cac12b
Ryan Strom <rstrom1@users.noreply.github.com>
2021-03-26 14:37:13 -0500

    Added .gitignore
    Added .gitignore in order to ignore the data directories.

--- Work_Node/Main.ps1
+++ Work_Node/Main.ps1
@@ -1,3 +1,3 @@
 ﻿#Send-Job -computers "C:\Strom\computers.txt" -outputURI "http://prprl-05teb9:8081/write"
 
-Get-Report -jsonDir "C:/users/ryan/documents/github/Personal-projects/work node/data" -exportCSV C:/strom/report.csv -adCacheFile D:/adcache.csv -base "McConnell AFB"
+Get-Report -jsonDir "C:/users/ryan/documents/github/Personal-projects/work_node/data" -exportCSV C:/strom/report.csv -adCacheFile D:/adcache.csv -base "McConnell AFB"
