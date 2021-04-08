d94af536038b26a382147b40f5fd895548018acd
Ryan Strom <rstrom1@users.noreply.github.com>
2021-04-07 23:26:29 -0500

    Began Meme Project
    Created basic MongoDB interaction.

---
+++ Meme_Program/app.js
@@ -0,0 +1,41 @@
+const fs = require("fs");
+const express = require("express");
+const app = express();
+const MongoClient = require("mongodb").MongoClient;
+
+//app.use(express.json());
+//app.listen(8081);
+
+url = "mongodb://localhost:27017/";
+
+async function get(dataBase, coll) {
+
+    MongoClient.connect(url, { useUnifiedTopology: true }, (err, db) => {
+        if (err) throw err;
+        var dbo = db.db(dataBase);
+
+        dbo.collection(coll).find({}, {}).toArray((err, result) => {
+            if (err) throw err;
+            console.log(result);
+            db.close();
+        });
+
+    });
+
+}
+
+async function insert(database, coll, myobj) {
+
+    MongoClient.connect(url, { useUnifiedTopology: true }, (err, db) => {
+        if (err) throw err;
+        var dbo = db.db(database);
+        dbo.collection(coll).insertOne(myobj, { unique: true }, (err, res) => {
+            if (err) throw err;;
+            console.log("Document Inserted");
+            db.close();
+        });
+    });
+};
+
+insert("mydb", 'customers', { userName: 'Rstrom1763', firstName: 'Ryan', lastName: 'Strom', phone: '816-787-7716', address: '53398 Lawrence Ct McConnell AFB KS 67221', email: 'rstrom1763@gmail.com', favoriteColor: 'Red' });
+get('mydb', 'customers');
