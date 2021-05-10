3339a06b190fceb55c3b817bb3e954cd2e7222bb
Ryan <48137622+rstrom1763@users.noreply.github.com>
2021-05-09 20:27:46 -0600

    Update Plex-Compare-exp.ps1
    Added exclude file functionality and calculation of needed storage for the missing movies.

--- Plex_Comparisons/Plex-Compare-exp.ps1
+++ Plex_Comparisons/Plex-Compare-exp.ps1
@@ -3,7 +3,8 @@ Function Compare-PlexDB {
     param(
 
         [Parameter(Mandatory = $true)][String]$csvDB1,
-        [Parameter(Mandatory = $true)][String]$csvDB2
+        [Parameter(Mandatory = $true)][String]$csvDB2,
+        [string]$excludeFile
 
     )
 
@@ -12,40 +13,53 @@ Function Compare-PlexDB {
     $need = @()
     Remove-Variable -Name csvdb1, csvdb2
 
-    $hash1 = @{}
-    $hash2 = @{}
+    $hash = @{}
+    $excludeHash = @{}
+    $i = 1
+    [int64]$spaceNeeded = 0
 
-    foreach ($entry in $csv1) {
-        if (!$hash1.ContainsKey($entry."Sort title")) {
-            $hash1.add($entry."Sort title", $entry."Part Size as Bytes")
+    if ($null -ne $excludeFile) {
+        if (Test-Path $excludeFile) {
+            foreach ($entry in (Get-Content $excludeFile)) {
+                $excludeHash.add(($entry), $i)
+                $i++
+            }
+        }
+        else {
+            Write-Host "$excludeFile does not exist."
         }
     }
     foreach ($entry in $csv2) {
-        if (!$hash2.ContainsKey($entry."Sort title")) {
-            $hash2.add($entry."Sort Title", $entry."Part Size as Bytes")
+        if (!$hash.ContainsKey($entry."Sort title")) {
+            $hash.add(($entry."Sort Title"), $entry."Part Size as Bytes")
         }
     }
-    
     foreach ($movie in $csv1) {
-        if (!$hash2.ContainsKey($movie."Sort title")) {
+        if (!$hash.ContainsKey($movie."Sort title") -and !$excludeHash.ContainsKey($movie."Sort title")) {
             $need += $movie
+            if ($movie."Part Size as Byte" -notlike "*-*") {
+                try {
+                    $spaceNeeded += [int64]$movie."Part Size as Bytes"
+                }
+                catch {
+                    Write-Host $_
+                    Write-Host $movie."Sort Title"
+                }
+            }
         }
     }
-    
+    $spaceNeeded = [math]::Round(($spaceNeeded / 1GB))
+    Write-Host ("Space needed: $spaceNeeded GB")
     $need | Write-Output
     #$csv1 | 
     #Select-Object "Sort title","Year","Bitrate","Video Resolution","Video Codec","Part Size","Part File","Media ID","Title","Duration","Added","Updated","Width","Height","Aspect Ratio","Audio Codec","Container","Part File Combined","Part File Path"
 
-    #Remove-Variable -Name csv1, csv2, hash1, hash2
+    #Remove-Variable -Name csv1, csv2, hash1, hash
 
 }
 
 Clear-Host
 
+Compare-PlexDB -csvDB1 "C:\Strom\kentLibrary.csv" -csvDB2 "C:/strom/RyanLibrary.csv" -excludeFile "C:/strom/exclude.txt" | Sort-Object -Unique -Property "Sort title" |  Export-Csv C:/strom/KentNoHave.csv -NoTypeInformation
 
-Compare-PlexDB -csvDB1 "C:\Strom\KentLibrary.csv" -csvDB2 "C:\Strom\ryanlibrary.csv" | Sort-Object -Unique -Property "Sort title" |  Export-Csv C:/strom/RyanNoHave.csv -NoTypeInformation
-
-Compare-PlexDB -csvDB1 "C:\Strom\ryanlibrary.csv" -csvDB2  "C:\Strom\KentLibrary.csv" | Sort-Object -Unique -Property "Sort title" |  Export-Csv C:/strom/KentNoHave.csv -NoTypeInformation
-
-
-#Compare-PlexDB -csvDB2 "C:\Strom\Movies.fazz.1 - Movies.fazz.1.csv" -csvDB1 "C:\Strom\MoviesLvl6Mar_23_2021.csv" | Export-Csv C:/strom/test2.csv
+Compare-PlexDB -csvDB1 "C:/strom/RyanLibrary.csv" -csvDB2 "C:\Strom\kentLibrary.csv" -excludeFile "C:/strom/exclude.txt" | Sort-Object -Unique -Property "Sort title" | Export-Csv C:/strom/RyanNoHave.csv -NoTypeInformation
