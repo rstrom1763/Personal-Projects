e84df4a2368c748674626ec346ee7976f8710542
Ryan <48137622+rstrom1763@users.noreply.github.com>
2021-05-11 06:48:10 -0600

    Create compounding_interest.py

---
+++ compounding_interest.py
@@ -0,0 +1,28 @@
+import matplotlib.pyplot as plt
+
+
+def calculate_interest(principal, rate, contribution):
+    return (principal + contribution + (principal + contribution) * (rate / 100))
+
+
+initial = 0
+principal = initial
+rate = 10
+contribution = 12000
+data = []
+years = range(40)
+for i in years:
+    principal = calculate_interest(principal, rate, contribution)
+    data.append(principal/1000000)
+
+print(round((data[len(data)-1]),3))
+
+plt.plot(years, data, linewidth=3.0, color="red")
+
+plt.axhline(y=0, linewidth=0.5, color="black")
+plt.title('\$' + str(contribution) + ' annually, ' +
+          str(rate) + ' percent annual interest')
+
+plt.xlabel('Year')
+plt.ylabel('Dollars (Millions)')
+plt.show()
