4d363f7c5b79274d7360050638e0b736d9c87bf4
Ryan Strom <rstrom1@users.noreply.github.com>
2021-06-21 21:00:41 -0500

    Added Picture Scripts
    Added scripts Get-Picks and Move-Pictures.

---
+++ picture_scripts/Move-Pictures.ps1
@@ -0,0 +1,22 @@
+﻿Function Move-Pictures {
+
+    [CmdletBinding()]
+    Param(
+
+        [Parameter(Mandatory = $true)][String]$FilePath
+
+    )
+    $originalPath = (Get-Location).path
+    Set-Location $FilePath
+
+    if (!(Test-Path "$FilePath/jpg") -and (Test-Path "*.jpg")) { New-Item -ItemType Directory jpg }
+    if (!(Test-Path "$FilePath/raw") -and (Test-Path "*.cr3")) { New-Item -ItemType Directory raw }
+    if (!(Test-Path "$FilePath/videos") -and (Test-Path "*.mp4")) { New-Item -ItemType Directory videos }
+
+    Get-ChildItem | Where-Object { $_.name -like "*.jpg" -or $_.name -like "*.jpeg" } | Move-Item -Destination ./jpg
+    Get-ChildItem | Where-Object { $_.name -like "*.cr3" } | Move-Item -Destination ./raw
+    Get-ChildItem | Where-Object { $_.name -like "*.mp4" } | Move-Item -Destination ./videos
+
+    Set-Location $originalPath
+
+}
