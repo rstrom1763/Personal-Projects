72a5e1a01cc9d8550c5e71da431f75bccbfdfc47
Ryan Strom <rstrom1@users.noreply.github.com>
2021-06-21 22:02:06 -0500

    Update Move-Pictures.ps1
    Updated Move-Pictures to support the use of PNG files.

--- picture_scripts/Move-Pictures.ps1
+++ picture_scripts/Move-Pictures.ps1
@@ -9,11 +9,11 @@
     $originalPath = (Get-Location).path
     Set-Location $FilePath
 
-    if (!(Test-Path "$FilePath/jpg") -and (Test-Path "*.jpg")) { New-Item -ItemType Directory jpg }
+    if (!(Test-Path "$FilePath/jpg") -and ((Test-Path "*.jpg") -or (Test-Path "*.png"))) { New-Item -ItemType Directory jpg }
     if (!(Test-Path "$FilePath/raw") -and (Test-Path "*.cr3")) { New-Item -ItemType Directory raw }
     if (!(Test-Path "$FilePath/videos") -and (Test-Path "*.mp4")) { New-Item -ItemType Directory videos }
 
-    Get-ChildItem | Where-Object { $_.name -like "*.jpg" -or $_.name -like "*.jpeg" } | Move-Item -Destination ./jpg
+    Get-ChildItem | Where-Object { $_.name -like "*.jpg" -or $_.name -like "*.jpeg" -or $_.name -like "*.png"} | Move-Item -Destination ./jpg
     Get-ChildItem | Where-Object { $_.name -like "*.cr3" } | Move-Item -Destination ./raw
     Get-ChildItem | Where-Object { $_.name -like "*.mp4" } | Move-Item -Destination ./videos
 
