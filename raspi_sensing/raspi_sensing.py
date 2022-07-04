f385e2a125ab317f7e9fe046a0c96f7f70a981a1
Ryan <48137622+rstrom1763@users.noreply.github.com>
2022-07-03 18:04:47 -0700

    Added rounding
    Added rounding to the pressure and humidity data points.

--- raspi_sensing/raspi_sensing.py
+++ raspi_sensing/raspi_sensing.py
@@ -6,7 +6,7 @@ sense.clear()
 temp_celcius = sense.get_temperature()
 temp_farenheit = round((temp_celcius * 9/5) + 32,2)
 
-pressure = sense.get_pressure()
-humidity = sense.get_humidity()
+pressure = round(sense.get_pressure(),2)
+humidity = round(sense.get_humidity(),2)
 
 print(temp_farenheit,pressure,humidity)
