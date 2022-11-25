bfed8459c33d30eb38f2aab40df450c76eba4081
Ryan <rstrom1763@gmail.com>
2022-11-24 23:28:22 -0600

    Added comments to raspi_sensing.py

--- raspi_sensing/raspi_sensing.py
+++ raspi_sensing/raspi_sensing.py
@@ -3,10 +3,17 @@ from sense_hat import SenseHat
 sense = SenseHat()
 sense.clear()
 
+# Get the temp
 temp_celcius = sense.get_temperature()
+
+# Convert temp to Farenheit
 temp_farenheit = round((temp_celcius * 9/5) + 32,2)
 
+# Get air pressure
 pressure = round(sense.get_pressure(),2)
+
+# Get humidity
 humidity = round(sense.get_humidity(),2)
 
+# Print out all the variables
 print(temp_farenheit,pressure,humidity)
