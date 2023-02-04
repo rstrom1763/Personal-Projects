e3b07377eded3439a2d7f572b72c097fb14d630d
Ryan <48137622+rstrom1763@users.noreply.github.com>
2023-02-03 22:18:50 -0600

    Ran auto format on raspi_sensing.py

--- raspi_sensing/raspi_sensing.py
+++ raspi_sensing/raspi_sensing.py
@@ -3,8 +3,8 @@ import requests
 import json
 import time
 
-auth_code = '' #Place holder for when tokens are implemented
-server = 'http://maul.lan:8090/posttemp' #The api url
+auth_code = ''  # Place holder for when tokens are implemented
+server = 'http://maul.lan:8090/posttemp'  # The api url
 
 sense = SenseHat()
 
@@ -14,26 +14,26 @@ while True:
     temp_celcius = sense.get_temperature()
 
     # Convert temp to Farenheit
-    temp_farenheit = round((temp_celcius * 9/5) + 32,2)
+    temp_farenheit = round((temp_celcius * 9/5) + 32, 2)
 
     # Get air pressure
-    pressure = round(sense.get_pressure(),2)
+    pressure = round(sense.get_pressure(), 2)
 
     # Get humidity
-    humidity = round(sense.get_humidity(),2)
+    humidity = round(sense.get_humidity(), 2)
 
     headers = {
-        'Content-Type':'application/json'
-    } #Headers for the post request
+        'Content-Type': 'application/json'
+    }  # Headers for the post request
 
     data = {
-        'auth-code':auth_code,
-        'temp':temp_farenheit,
-        'humidity':humidity,
-        'pressure':pressure
+        'auth-code': auth_code,
+        'temp': temp_farenheit,
+        'humidity': humidity,
+        'pressure': pressure
     }
 
-    #Send post request to the server
-    requests.post(server,data=json.dumps(data),headers=headers)
+    # Send post request to the server
+    requests.post(server, data=json.dumps(data), headers=headers)
 
-    time.sleep(60) #Wait one minute before logging the temperature again
+    time.sleep(60)  # Wait one minute before logging the temperature again
