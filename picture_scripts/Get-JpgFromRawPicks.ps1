257361ed5c3425f36f8c696c0bf22bf3502ca752
Ryan <48137622+rstrom1763@users.noreply.github.com>
2024-07-17 19:34:08 -0600

    Fixed Get-JpgFromRawPicks.ps1
    This script previously was created for a specific task. It is now generalized and actually behaves as the name would suggest.

--- picture_scripts/Get-JpgFromRawPicks.ps1
+++ picture_scripts/Get-JpgFromRawPicks.ps1
@@ -2,18 +2,19 @@ Function Match-Photos {
 
     [CmdletBinding()]
     Param(
-
-        [Parameter(Mandatory = $true)][String]$SourcePath,
+        
+        [Parameter(Mandatory = $true)][String]$rawSourcePath,
+        [Parameter(Mandatory = $true)][String]$jpgSourcePath,
         [Parameter(Mandatory = $true)][String]$DestinationPath
 
     )
 
-    $files = Get-Content $SourcePath
+    $jpgFiles = Get-ChildItem $jpgSourcePath
 
-    foreach ($file in (Get-ChildItem $DestinationPath)) {
-        foreach ($file2 in $files) {
-            if ((($file.name) -replace $file.Extension,"") -like (($file2.name) -replace $file2.Extension,"")) {
-                Copy-Item -LiteralPath $file.Name -Destination 'C:/strom/photos/chief_mann_retirement/file2s/file/' -WhatIf
+    foreach ($rawFile in (Get-ChildItem $rawSourcePath)) {
+        foreach ($jpgFile in $jpgFiles) {
+            if ((($rawFile.name) -replace $rawFile.Extension,"") -like (($jpgFile.name) -replace $jpgFile.Extension,"")) {
+                Copy-Item $jpgFile.FullName -Destination $DestinationPath
             }
         }
     }
