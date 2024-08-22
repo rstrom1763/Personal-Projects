2e3cefecd75ec92a3bdf0d5845c004df2a1f5e25
Ryan <48137622+rstrom1763@users.noreply.github.com>
2024-08-21 23:32:12 -0600

    Renamed to Match-Jpg; Added Match-Raw
    There are now two separate functions. One does jpg to raw, the other does raw to jpg.

--- picture_scripts/Get-JpgFromRawPicks.ps1
+++ picture_scripts/Get-JpgFromRawPicks.ps1
@@ -1,4 +1,4 @@
-Function Match-Photos {
+Function Match-Jpg {
 
     [CmdletBinding()]
     Param(
@@ -15,6 +15,28 @@ Function Match-Photos {
         foreach ($jpgFile in $jpgFiles) {
             if ((($rawFile.name) -replace $rawFile.Extension,"") -like (($jpgFile.name) -replace $jpgFile.Extension,"")) {
                 Copy-Item $jpgFile.FullName -Destination $DestinationPath
+            }
+        }
+    }
+}
+
+Function Match-Raw {
+
+    [CmdletBinding()]
+    Param(
+        
+        [Parameter(Mandatory = $true)][String]$rawSourcePath,
+        [Parameter(Mandatory = $true)][String]$jpgSourcePath,
+        [Parameter(Mandatory = $true)][String]$DestinationPath
+
+    )
+
+    $rawFiles = Get-ChildItem $rawSourcePath
+
+    foreach ($jpgFile in (Get-ChildItem $jpgSourcePath)) {
+        foreach ($rawFile in $rawFiles) {
+            if ((($jpgFile.name) -replace $jpgFile.Extension,"") -like (($rawFile.name) -replace $rawFile.Extension,"")) {
+                Copy-Item $rawFile.FullName -Destination $DestinationPath
             }
         }
     }
